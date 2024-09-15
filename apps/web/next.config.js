de8397ef037df9e12a56d82f5348ee3eb862557f
henryliu <17600047539@163.com>
2024-09-15 23:36:04 +0800

    update

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,18 +3,18 @@
 
 const nextConfig = {
   // distDir: 'dist',
-  async rewrites() {
-    return [
-      {
-        source: '/sov-http',
-        destination: 'http://localhost:12346/',
-      },
-      {
-        source: '/sov-rpc',
-        destination: 'http://localhost:12345/',
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/sov-http',
+  //       destination: 'http://localhost:12346/',
+  //     },
+  //     {
+  //       source: '/sov-rpc',
+  //       destination: 'http://localhost:12345/',
+  //     },
+  //   ]
+  // },
 };
 
 module.exports = {
